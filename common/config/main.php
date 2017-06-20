ac227df916f17a8898a6bb62c6b7890dc3a643f5
Jovi <redzjovi@gmail.com>
2017-06-20 10:10:28 +0700

    Add Internationalization.

--- common/config/main.php
+++ common/config/main.php
@@ -1,10 +1,20 @@
 <?php
 return [
+    'language' => 'en',
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
         'authManager' => ['class' => 'dektrium\rbac\components\DbManager'],
         'cache' => [
             'class' => 'yii\caching\FileCache',
+        ],
+        'i18n' => [
+            'translations' => [
+                'app*' => [
+                    'basePath' => '@common/messages',
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'fileMap' => ['app' => 'app.php'],
+                ],
+            ],
         ],
         'urlManager' => [
             'enablePrettyUrl' => true,
